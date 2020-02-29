c6dba03250a2b11d16e5b4ebede367fd90443da3
RendL <farukh-999@mail.ru>
2020-02-29 12:40:16 +0500

    Done

--- server/server.go
+++ server/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -11,6 +10,7 @@ import (
 )
 
 func main() {
+	Init()
 	file, err := os.OpenFile("server-log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
 		fmt.Printf("can't open log file %e", err)
@@ -67,73 +67,12 @@ func handleConnection(conn net.Conn) {
 	readString, _ := reader.ReadString('\n')
 	split := strings.Split(strings.TrimSpace(readString), " ")
 	if len(split) != 3 {
-		log.Fatal("")
+		log.Print("incorrect request")
+		return
 	}
-	meth, requet, protocol := split[0], split[1], split[2]
+	meth, request, protocol := split[0], split[1], split[2]
 	if meth == "GET" && protocol == "HTTP/1.1" {
-		switch requet {
-		case "/":
-			all, _ := ioutil.ReadFile("index.html")
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("HTTP/1.1 200 OK\r\n")
-			writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(all)))
-			writer.WriteString("Content-Type: text/html\r\n")
-			writer.WriteString("Connection: Close\r\n")
-			writer.WriteString("\r\n")
-			writer.Write(all)
-			writer.Flush()
-		case "/1.png":
-			all, _ := ioutil.ReadFile("1.png")
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("HTTP/1.1 200 OK\r\n")
-			writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(all)))
-			writer.WriteString("Content-Type: image/png\r\n")
-			writer.WriteString("Connection: Close\r\n")
-			writer.WriteString("\r\n")
-			writer.Write(all)
-			writer.Flush()
-		case "/2.jpg":
-			all, _ := ioutil.ReadFile("2.jpg")
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("HTTP/1.1 200 OK\r\n")
-			writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(all)))
-			writer.WriteString("Content-Type: image/jpg\r\n")
-			writer.WriteString("Connection: Close\r\n")
-			writer.WriteString("\r\n")
-			writer.Write(all)
-			writer.Flush()
-		case "/index-html.html":
-			all, _ := ioutil.ReadFile("index-html.html")
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("HTTP/1.1 200 OK\r\n")
-			writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(all)))
-			writer.WriteString("Content-Type: text/html\r\n")
-			writer.WriteString("Connection: Close\r\n")
-			writer.WriteString("\r\n")
-			writer.Write(all)
-			writer.Flush()
-		case "/123.txt":
-			all, _ := ioutil.ReadFile("123.txt")
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("HTTP/1.1 200 OK\r\n")
-			writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(all)))
-			writer.WriteString("Content-Disposition: attachment; filename=down.txt\r\n")
-			writer.WriteString("Connection: Close\r\n")
-			writer.WriteString("\r\n")
-			writer.Write(all)
-			writer.Flush()
-		case "/1.pdf":
-			all, _ := ioutil.ReadFile("1.pdf")
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("HTTP/1.1 200 OK\r\n")
-			writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(all)))
-			writer.WriteString("Content-Type: application/pdf\r\n")
-			writer.WriteString("Connection: Close\r\n")
-			writer.WriteString("\r\n")
-			writer.Write(all)
-			writer.Flush()
-		}
-
+		AnswerToHttp(request, conn)
 	}
 
 }
